14c5f027ad913d622e601d65af3d0658ccb09f57
Ahmed Fouad LAGHA <107846932+ahmedfouadlagha@users.noreply.github.com>
2024-03-12 21:49:48 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""__init__ method for models package, or
-Module for FileStorage autoinit."""
+"""
+__init__ method for models package, or Module for FileStorage autoinit.
+__init__ magic method for models directory
+"""
 from models.engine.file_storage import FileStorage
 
 
